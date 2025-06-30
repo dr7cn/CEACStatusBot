442e022a9171cb4f3c685b2cae1605310f109949
dr7cn <135789622+dr7cn@users.noreply.github.com>
2025-06-30 11:41:49 +0100

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -1,34 +1,21 @@
 import os
-from CEACStatusBot import NotificationManager,EmailNotificationHandle,TelegramNotificationHandle
-
+from CEACStatusBot import NotificationManager, TelegramNotificationHandle
 
 try:
-    LOCATION = os.environ["LOCATION"]
     NUMBER = os.environ["NUMBER"]
-    PASSPORT_NUMBER = os.environ["PASSPORT_NUMBER"]
-    SURNAME = os.environ["SURNAME"]
-    notificationManager = NotificationManager(LOCATION,NUMBER,PASSPORT_NUMBER,SURNAME)
+    notificationManager = NotificationManager(NUMBER)
 except KeyError:
-    print("LOCATION or NUMBER Error")
-
-try:
-    FROM = os.environ["FROM"]
-    TO = os.environ["TO"]
-    PASSWORD = os.environ["PASSWORD"]
-    SMTP = os.environ.get("SMTP", "")
-    if FROM and TO and PASSWORD:
-        emailNotificationHandle = EmailNotificationHandle(FROM,TO,PASSWORD,SMTP)
-        notificationManager.addHandle(emailNotificationHandle)
-except KeyError:
-    print("Email notification config error")
+    print("NUMBER is missing in environment variables")
+    exit(1)
 
 try:
     BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
     CHAT_ID = os.environ["TG_CHAT_ID"]
     if BOT_TOKEN and CHAT_ID:
-        tgNotif = TelegramNotificationHandle(BOT_TOKEN,CHAT_ID)
+        tgNotif = TelegramNotificationHandle(BOT_TOKEN, CHAT_ID)
         notificationManager.addHandle(tgNotif)
 except KeyError:
     print("Telegram bot notification config error")
+    exit(1)
 
 notificationManager.send()
